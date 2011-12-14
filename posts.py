f7a25c3f26b544a05e7f0bb21c043d89d9e8d3ee
Jun Mukai <mukai@jmuk.org>
2011-12-14 21:26:36 +0900

    Extract subjects in processPost()

--- posts.py
+++ posts.py
@@ -14,6 +14,19 @@ def isMeaningfulPost(post):
 
   return isMeaningfulContent(re.sub('<.*?>', '', post['object']['content']))
 
+def extractSubject(post):
+  """Extract the title from the specified post and stores to it back."""
+  lines = post['object']['content'].split(r"<br />")
+  for line in lines:
+    line = re.sub('<a .*?</a>', '', line).strip()
+    if len(line) == 0:
+      continue
+    (prefix, segment, remaining) = line.partition(u'\u3002')
+    post['object']['subject'] = prefix + segment
+    return
+  # if it fails to extract the subject, use 'title' instead
+  post['object']['subject'] = post['title']
+
 def formAttachments(post):
   attachments = post['object'].get('attachments', [])
   attachments_len = len(attachments)
@@ -42,3 +55,4 @@ def formAttachments(post):
 def processPost(post):
   post['self_url'] = SERVER_ROOT + url_for('.get_post', activity_id=post['id'])
   formAttachments(post)
+  extractSubject(post)
